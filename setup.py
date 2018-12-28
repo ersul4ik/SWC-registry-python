7c84353bf61bf81029fb324c4989c6912f7c0fb1
ersul4ik <ersul4ik@mail.ru>
2018-12-28 18:15:58 +0600

    Changed config.yml for set env var

--- setup.py
+++ setup.py
@@ -7,20 +7,7 @@ from setuptools import setup
 from setuptools.command.install import install
 
 # circleci.py version
-VERSION = "0.0.5"
-
-class VerifyVersionCommand(install):
-    """Custom command to verify that the git tag matches our version"""
-    description = 'verify that the git tag matches our version'
-
-    def run(self):
-        tag = os.getenv('CIRCLE_TAG')
-
-        if tag != VERSION:
-            info = "Git tag: {0} does not match the version of this app: {1}".format(
-                tag, VERSION
-            )
-            sys.exit(info)
+VERSION = os.getenv('CIRCLE_TAG')
 
 setup(
     name="swc-registry",
